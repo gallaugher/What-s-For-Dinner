adcd607a0ac4a27dd99094de15e5e5e3a0581637
John Gallaugher <john.gallaugher@bc.edu>
2018-02-05 17:05:33 -0500

    Initial Commit

---
+++ What's For Dinner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's For Dinner
+//
+//  Created by John Gallaugher on 2/5/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
