ea574ddbf191b1edd9bf02ad697b086e6a978fa8
John Gallaugher <john.gallaugher@bc.edu>
2018-02-05 19:28:42 -0500

    Updated with constraints & to show non-repeating images using a while loop. See class slides for repeat-while example.

--- What's For Dinner/ViewController.swift
+++ What's For Dinner/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dinnerImageView: UIImageView!
+    @IBOutlet weak var dinnerMessage: UILabel!
+    var lastMeal = -1 // a class-wide variable, or a property of the ViewController.swift class. Since it was defined within the class curlies, even with the functions, it's available to "ALL" functions and will hold its value, even after it has been updated inside of a function.
+    
+    let choicesArray = ["Burrito",
+                        "Cobb Salad",
+                        "Pepperoni Pizza",
+                        "Sushi"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func dinnerButtonPressed(_ sender: UIButton) {
+        
+        var selectedMeal = Int(arc4random_uniform(UInt32(choicesArray.count)))
+        
+        while selectedMeal == lastMeal {
+            // Generate a new random Int for selectedMeal...
+            selectedMeal = Int(arc4random_uniform(UInt32(choicesArray.count)))
+        }
+        
+        dinnerImageView.image = UIImage(named: choicesArray[selectedMeal])
+        dinnerMessage.text = choicesArray[selectedMeal]
+        lastMeal = selectedMeal // Remember the lastMeal in the global var
+        
+        // Original answer, that could show repeats each time clicked, is commented out, below:
+//        let selectedMeal = Int(arc4random_uniform(UInt32(choicesArray.count)))
+//        let choice = choicesArray[selectedMeal]
+//        dinnerMessage.text = choice
+//        dinnerImageView.image = UIImage(named: choice)
     }
-
+    
 
 }
 
